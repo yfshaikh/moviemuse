9e6ce2a4e377dd5f42c8ab1d7fefbab38b45157d
Yusuf <yshaikh113@outlook.com>
2024-11-09 15:12:43 -0600

    updated api

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,6 +1,6 @@
 const API_BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:8000/'
-  : 'https://yfshaikh.com/api';
+  : 'https://moviemuseserver.vercel.app';
   
 
 export default API_BASE_URL;
