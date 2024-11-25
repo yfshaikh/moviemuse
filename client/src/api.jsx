f9d10362722dcb947405e0ce9313fc730692c631
Yusuf <yshaikh113@outlook.com>
2024-11-25 13:01:54 -0600

    bug fix

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:8000';
-  
+const API_BASE_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:8000'
+  : 'https://moviemuseserver.vercel.app';
 
 export default API_BASE_URL;
